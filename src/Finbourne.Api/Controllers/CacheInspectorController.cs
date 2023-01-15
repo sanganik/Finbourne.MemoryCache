7d4127e043c251027e8187e6cf56ba02c5c9a4b0
Kushal Sangani <kushal.sangani@bourne-leisure.co.uk>
2023-01-15 20:09:25 +0000

    Created Baseline project

---
+++ src/Finbourne.Api/Controllers/CacheInspectorController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Finbourne.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CacheInspectorController : ControllerBase
+    {
+        public CacheInspectorController()
+        {
+
+        }
+
+        [HttpGet]
+        [Route("Get")]
+        public async Task<ActionResult> Get(string key)
+        {
+            return Ok($"Inspected Key is {key}");
+        }
+    }
+}
---
+++ src/Finbourne.Api/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+using System.IO;
+
+namespace Finbourne.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                   .UseStartup<Startup>()
+                   .ConfigureServices((httpContext, services) =>
+                   {
+
+                       // setup swagger
+                       services.AddSwaggerGen(c =>
+                       {
+                           c.SwaggerDoc("v1", new OpenApiInfo { Title = "Finbourne memory cache component api", Version = "v1" });
+                       });
+
+                       services.AddMvcCore()
+                               .AddFormatterMappings()
+                               .AddApiExplorer()
+                               .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+                   });
+    }
+}
